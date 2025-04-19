5df14bea71a1d6bd796c5b965d635b5839de28f6
Teryan-Bor <boristeryan10@gmail.com>
2025-04-19 16:49:56 +0400

    исправ. ошибок

--- app/components/Shared/menu.tsx
+++ app/components/Shared/menu.tsx
@@ -14,12 +14,12 @@ export const Menu = () => {
 
     return (
 
-        <><div className="menu_container max-w-[500px] w-full mx-auto fixed bottom-0 left-2/4 translate-x-[-50%] bg-[#2D2D4F] rounded-t-[16px] flex justify-between items-center py-[22px] px-[20px]">
+        <><div className="menu_container max-w-[500px] w-full mx-auto fixed bottom-0 left-2/4 translate-x-[-50%] bg-[#2D2D4F] rounded-t-[16px] cursor-pointer font-normal text-[1.43em] flex justify-between items-center py-[22px] px-[20px]">
             {Object.keys(PageConfig).map((key) => (
                 <div className={`${active == key ? 'Active' : 'Disable'} `} key={key}>{key}</div>
             ))}
         </div>
-            <div className="max-w-[380px] w-full max-h-[53px] h-full absolute top-0.5 bg-[#2D2D4F] rounded-[13px] mx-auto left-2/4 translate-x-[-50%] text-center text-white font-normal text-[3em] flex justify-center items-center">Добавить задачу</div></>
+            <div className="max-w-[380px] w-full max-h-[53px] h-full absolute top-0.5 bg-[#2D2D4F] rounded-[13px] mx-auto left-2/4 translate-x-[-50%] text-center text-white font-normal text-[1.6875em] flex justify-center items-center">Добавить задачу</div></>
 
 
 
