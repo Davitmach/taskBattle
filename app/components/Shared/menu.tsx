366b480afdd8fc489b591d8df8bc40a8a2c154cd
Davitmach <wvime30@gmail.com>
2025-04-19 14:40:25 +0400

    refactor: standardize keys in PageConfig and improve Menu component logic

--- app/components/Shared/menu.tsx
+++ app/components/Shared/menu.tsx
@@ -1,12 +1,24 @@
-import { PageConfig } from "@/app/config/pageConfig"
+'use client';
+import { PageConfig } from "@/app/config/pageConfig";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 export const Menu = () => {
-    
+    const Path = usePathname();
+    const keys = Object.keys(PageConfig) as Array<keyof typeof PageConfig>;
+    const [active, setActive] = useState<keyof typeof PageConfig>(keys[1]);
+
+    useEffect(() => {
+        const found = keys.find((key) => PageConfig[key] === Path);
+        if (found) setActive(found);
+    }, [Path]);
+
     return(
-        <div className="">
+        <div className="max-w-[500px] w-full mx-auto fixed bottom-0 left-2/4 translate-x-[-50%] bg-[#2D2D4F] rounded-t-[16px] flex justify-between items-center py-[22px] px-[20px]">
 {Object.keys(PageConfig).map((key)=> (
-    <div key={key}>{key}</div>
+    <div  className={`${active == key ? 'Active' :'Disable'}`}   key={key}>{key}</div>
 ))}
-99999999
+
         </div>
     )
+
 }
