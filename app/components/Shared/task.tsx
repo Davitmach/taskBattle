22e245fc400ae27c4a25ef0c07fbcbc4e1fc7f34
Davitmach <wvime30@gmail.com>
2025-04-24 18:17:51 +0400

    вуф

--- app/components/Shared/task.tsx
+++ app/components/Shared/task.tsx
@@ -1,92 +1,92 @@
-// 'use client';
+'use client';
 
-// import { useSearchParams, useRouter } from 'next/navigation';
-// import { useEffect, useState } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-// export default function ModalManager() {
-//   const searchParams = useSearchParams();
-//   const router = useRouter();
+export default function ModalManager() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
-//   const modal = searchParams.get('modal');
-//   const title = searchParams.get('title');
-//   const type = searchParams.get('type');
-//   const date = searchParams.get('date');
-//   const friendsRaw = searchParams.get('friends');
+  const modal = searchParams.get('modal');
+  const title = searchParams.get('title');
+  const type = searchParams.get('type');
+  const date = searchParams.get('date');
+  const friendsRaw = searchParams.get('friends');
 
-//   const [isOpen, setIsOpen] = useState(false);
-//   const [friends, setFriends] = useState<{ name: string; img: string; total: number }[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [friends, setFriends] = useState<{ name: string; img: string; total: number }[]>([]);
 
-//   useEffect(() => {
-//     setIsOpen(!!modal);
+  useEffect(() => {
+    setIsOpen(!!modal);
 
-//     if (friendsRaw) {
-//       try {
-//         const parsed = JSON.parse(decodeURIComponent(friendsRaw));
-//         if (Array.isArray(parsed)) {
-//           setFriends(parsed);
-//         }
-//       } catch (err) {
-//         console.error('Ошибка парсинга friends:', err);
-//       }
-//     }
-//   }, [modal, friendsRaw]);
+    if (friendsRaw) {
+      try {
+        const parsed = JSON.parse(decodeURIComponent(friendsRaw));
+        if (Array.isArray(parsed)) {
+          setFriends(parsed);
+        }
+      } catch (err) {
+        console.error('Ошибка парсинга friends:', err);
+      }
+    }
+  }, [modal, friendsRaw]);
 
-//   const closeModal = () => {
-//     const newParams = new URLSearchParams(searchParams.toString());
-//     newParams.delete('modal');
-//     newParams.delete('title');
-//     newParams.delete('friends');
-//     newParams.delete('type');
-//     newParams.delete('date');
-//     router.push('/');
-//   };
+  const closeModal = () => {
+    const newParams = new URLSearchParams(searchParams.toString());
+    newParams.delete('modal');
+    newParams.delete('title');
+    newParams.delete('friends');
+    newParams.delete('type');
+    newParams.delete('date');
+    router.push('/');
+  };
 
-//   if (!isOpen) return null;
+  if (!isOpen) return null;
 
-//   return (
-//     <div className=" fixed inset-0 z-50 flex items-center justify-center bg-[#1E1E2FBF]">
-//       <div className="modal_page bg-[#2D2D4F] py-[19px] px-[15px] rounded-[16px]  w-full max-w-[400px] relative flex flex-col justify-between">
+  return (
+    <div className=" fixed inset-0 z-50 flex items-center justify-center bg-[#1E1E2FBF]">
+      <div className="modal_page bg-[#2D2D4F] py-[19px] px-[15px] rounded-[16px]  w-full max-w-[400px] relative flex flex-col justify-between">
 
-//       <svg className='absolute right-[15px] top-[15px] cursor-pointer' onClick={closeModal} width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-// <path d="M2 2L17 17M17 2L2 17" stroke="white" strokeWidth="3"/>
-// </svg>
-// <div>
-//         <h2 className='text-[#F1F1F1] text-[2.36em] font-[400] text-center'>{title}</h2>
-//         <p className='text-[#F1F1F1] text-[1.43em] font-[400] text-center'>{type}</p>
+      <svg className='absolute right-[15px] top-[15px] cursor-pointer' onClick={closeModal} width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M2 2L17 17M17 2L2 17" stroke="white" strokeWidth="3"/>
+</svg>
+<div>
+        <h2 className='text-[#F1F1F1] text-[2.36em] font-[400] text-center'>{title}</h2>
+        <p className='text-[#F1F1F1] text-[1.43em] font-[400] text-center'>{type}</p>
         
 
 
-//         {friends.length > 0 && (
-//           <div className="mt-4 mb-[10px]">
-//             <ul className="mt-1 space-y-[8px] max-h-[140px] overflow-y-auto">
-//               {friends.map((friend, index) => (
-//                 <li key={index} className="flex items-center gap-2 bg-[#1E1E2F] rounded-[13px] p-[9px] ">
-//                  <div><img src={friend.img} className='rounded-[50px]' width={43} height={43} /></div>
-//                  <div className='flex flex-col'>
-//                   <span className='text-[1em] text-[#F1F1F1] font-[400]'>{friend.name}</span>
-//                   <span className='text-[1em] text-[#F1F1F1] font-[400]'>Total Tasks:{friend.total}</span>
-//                  </div>
-//                 </li>
-//               ))}
-//             </ul>
-//           </div>
-//         )}
-//         <span className='text-white text-[1em] font-[400] '>Дата окончания: {date}</span>
-//         </div>
+        {friends.length > 0 && (
+          <div className="mt-4 mb-[10px]">
+            <ul className="mt-1 space-y-[8px] max-h-[140px] overflow-y-auto">
+              {friends.map((friend, index) => (
+                <li key={index} className="flex items-center gap-2 bg-[#1E1E2F] rounded-[13px] p-[9px] ">
+                 <div><img src={friend.img} className='rounded-[50px]' width={43} height={43} /></div>
+                 <div className='flex flex-col'>
+                  <span className='text-[1em] text-[#F1F1F1] font-[400]'>{friend.name}</span>
+                  <span className='text-[1em] text-[#F1F1F1] font-[400]'>Total Tasks:{friend.total}</span>
+                 </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <span className='text-white text-[1em] font-[400] '>Дата окончания: {date}</span>
+        </div>
 
-// <div className='flex w-full justify-center gap-[13px] mt-[20px]'>
-// <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-// <rect width="50" height="50" rx="8" fill="#A2E9BA"/>
-// <path d="M10 25L20 35L40 15" stroke="white" strokeWidth="6"/>
-// </svg>
-// <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-// <rect width="50" height="50" rx="8" fill="#BE3A50" fillOpacity="0.85"/>
-// <path d="M10 10L40 40M40 10L10 40" stroke="white" strokeWidth="6"/>
-// </svg>
+<div className='flex w-full justify-center gap-[13px] mt-[20px]'>
+<svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="50" height="50" rx="8" fill="#A2E9BA"/>
+<path d="M10 25L20 35L40 15" stroke="white" strokeWidth="6"/>
+</svg>
+<svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="50" height="50" rx="8" fill="#BE3A50" fillOpacity="0.85"/>
+<path d="M10 10L40 40M40 10L10 40" stroke="white" strokeWidth="6"/>
+</svg>
 
 
-// </div>
-//       </div>
-//     </div>
-//   );
-// }
+</div>
+      </div>
+    </div>
+  );
+}
