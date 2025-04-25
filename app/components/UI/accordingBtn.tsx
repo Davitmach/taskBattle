d54dce3beb2d3c5f2bd20a12f7aa362bd525ca87
Davitmach <wvime30@gmail.com>
2025-04-25 18:48:56 +0400

    fix: correct innerColor prop usage in AccordingBtn component

--- app/components/UI/accordingBtn.tsx
+++ app/components/UI/accordingBtn.tsx
@@ -6,7 +6,7 @@ export const AccordingBtn = (props:IAccordingBtnProps) => {
 
     if(props.state == false) return <svg className="cursor-pointer" onClick={props.onClick} width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect width="15" height="15" rx="3" fill={props.color}/>
-    <path d="M11 10H4L7.5 5L11 10Z" fill={props.innerColor ? props.innerColor:'"#D9D9D9"'}/>
+    <path d="M11 10H4L7.5 5L11 10Z" fill={props.innerColor ? props.innerColor:'#D9D9D9'}/>
     </svg>
     
     else return(
