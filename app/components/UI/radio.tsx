3f87d58fede5b77e3209f3245355098c487a6b99
Davitmach <wvime30@gmail.com>
2025-07-24 11:13:51 +0400

    1

--- app/components/UI/radio.tsx
+++ app/components/UI/radio.tsx
@@ -6,7 +6,7 @@ export const Radio = (props:IRadio)=> {
     }
     return(
         <div className="flex items-center gap-[8px]"  onClick={HandleActive}>
-            <div className="w-[20px] h-[20px] rounded-[100px] bg-[#1E1E2F] cursor-pointer flex items-center justify-center">{props.active == props.text &&<div className="w-[10px] h-[10px] rounded-[100px] bg-[#7F5AF0]"></div>}</div>
+            <div className="w-[20px] h-[20px] duration-[400ms] active:scale-[0.9] rounded-[100px] bg-[#1E1E2F] cursor-pointer flex items-center justify-center">{props.active == props.text &&<div className="w-[10px] h-[10px] rounded-[100px] bg-[#7F5AF0]"></div>}</div>
             <div className="text-[#FFFFFF] flex gap-[4px] text-[15px]">{props.text}</div>
         </div>
     )
