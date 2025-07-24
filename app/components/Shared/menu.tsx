8a6775d33c6040a48b651a966a07ca9f9832b411
Davitmach <wvime30@gmail.com>
2025-07-24 16:52:51 +0400

    1

--- app/components/Shared/menu.tsx
+++ app/components/Shared/menu.tsx
@@ -28,7 +28,7 @@ if(Path.includes('newtask')) return null
           datatype={path}
           className={`${
             active === key ? 'Active' : 'Disable'
-          } font-[400] text-[1.43em] cursor-pointer`}
+          } font-[400] text-[1.43em] cursor-pointer duration-[400ms] active:scale-[0.9]`}
           key={key}
           onClick={() => Router(PageConfig[key])} 
         >
