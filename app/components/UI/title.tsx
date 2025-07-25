6c57207c8239e90a1315ecde2d70fca73807adc3
Davitmach <wvime30@gmail.com>
2025-07-25 12:46:48 +0400

    1

--- app/components/UI/title.tsx
+++ app/components/UI/title.tsx
@@ -2,6 +2,6 @@ import { ITitleProps } from "@/app/types/title";
 
 export const Title = (props:ITitleProps)=> {
 return(
-    <h1 style={{color:props.color}} className={props?.className}>{props.children}</h1>
+    <h1 style={{color:props.color}} className={`${props?.className} no-select`}>{props.children}</h1>
 )
 }
